c2f27c12ac8a7d2fe4eb3956a4299fcf818ff77f
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2023-01-04 00:35:51 +0530

    #include<stdio.h> int main() {     int n,i,v,r;     scanf("%d%d",&n,&r);     for(i=1;i<=r;i++)     {         if(i%2!=0)         {             v=n*i;             printf("%d x %d = %d ",n,i,v);         }     } }

---
+++ Tables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,v,r;
+    scanf("%d%d",&n,&r);
+    for(i=1;i<=r;i++)
+    {
+        if(i%2!=0)
+        {
+            v=n*i;
+            printf("%d x %d = %d
+",n,i,v);
+        }
+    }
+}
