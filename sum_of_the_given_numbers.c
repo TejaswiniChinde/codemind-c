d3079838c1e1e6d4da4b589212962d0f3a76e323
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2023-01-03 23:05:55 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int a,b,c;         scanf("%d%d",&a,&b);         c=a+b;         printf("%d ",c);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a,b,c;
+        scanf("%d%d",&a,&b);
+        c=a+b;
+        printf("%d
+",c);
+    }
+}
